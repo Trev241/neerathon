e5a5944dc303729675d43c6fde9155087ee35a14
Trev241 <trevisliu@gmail.com>
2023-02-01 22:22:56 +0530

    Missing path module imported

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 
 const cors = require("cors")
 require("dotenv").config()
@@ -7,11 +8,11 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static("public"))
 app.use(require("./routes/record"))
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html', {root: this.path.join(__dirname, 'public')})
+  res.sendFile("index.html", {root: path.join(__dirname, "public")})
 })
 
 // get driver connection
