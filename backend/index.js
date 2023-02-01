0ade12a54bf650c649f885450785b191db43a7a2
Trev241 <trevisliu@gmail.com>
2023-02-01 21:57:30 +0530

    Environment path changed

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 const cors = require("cors")
-require("dotenv").config({ path: "./config.env" })
+require("dotenv").config()
 const port = process.env.PORT || 5000
 
 app.use(cors())
